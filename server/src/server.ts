09f0e5b0f751e5e767727eb8f6a592d029c68886
Achy <achytimis@gmail.com>
2022-02-28 00:43:07 +0200

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import { app } from "./app";
 const port = process.env.PORT || 4000;
 
-console.log(process.env);
 try {
   app.listen(port, (): void => {
     console.log(`Connected successfully on port ${port}`);
